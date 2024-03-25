f284e25ce1ce16e8f89331cf80339d80d3706ece
Tim Heilig <theilig@box.com>
2024-03-25 09:32:37 -0700

    FIx our change to by 8.2 compliant

--- src/Phake/Stubber/Answers/ParentDelegate.php
+++ src/Phake/Stubber/Answers/ParentDelegate.php
@@ -89,7 +89,7 @@ class ParentDelegate implements \Phake\Stubber\IAnswer
             $reflMethod = $reflParent->getMethod($method);
 
             if (!$reflMethod->isAbstract()) {
-                return ['parent', $method];
+                return [$reflParent->getName(), $method];
             }
         } catch (\ReflectionException $e) {
         }
